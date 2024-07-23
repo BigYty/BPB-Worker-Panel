07a1663b0c7db551310a72f4630edd8dcd0dbc02
BigYty <35890111+BigYty@users.noreply.github.com>
2024-07-23 17:24:39 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b05a82e0-89a9-4f9a-b53c-ca72391ffcad';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
